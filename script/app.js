a81122e6af42e4430d9e400114f67cd0dea07a65
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2022-04-27 08:26:18 +0900

    Delete script directory

--- script/app.js
+++
@@ -1,9 +0,0 @@
-const vendingBtn = document.querySelectorAll(".btn");
-
-console.log(vendingBtn);
-
-for (let btn of vendingBtn) {
-  btn.addEventListener("click", () => {
-    console.log(btn);
-  });
-}
